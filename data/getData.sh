f58de9182c5cb3c7c80f3820c227d6db9ff1df50
Jiameng Fan <jmfan@bu.edu>
2020-02-05 21:41:00 -0500

    scene graph

--- data/getData.sh
+++ data/getData.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 wget https://nlp.stanford.edu/data/gqa/data1.2.zip
 wget http://nlp.stanford.edu/data/glove.6B.zip
+wget https://nlp.stanford.edu/data/gqa/sceneGraphs.zip
 unzip data1.2.zip
 unzip glove.6B.zip
+unzip sceneGraphs.zip
 rm data1.2.zip
 rm glove.6B.zip
+rm sceneGraphs.zip
